f09b3953a557a9b2a8028cd3571a2b9ae52b174c
justmarkup <hallo@justmarkup.com>
2014-05-12 19:36:29 +0200

    clean up, fix typo

--- maphancement.js
+++ maphancement.js
@@ -1,14 +1,14 @@
 /*Maphancement - Google Maps (progressive enhanced) - At first load only the static image of the map is loaded, once the user interacts with the map the iframe with the dynamic map is loaded Author: Michael Scharnagl, @justmarkup, 2014 | License: MIT */
 
 /*global document*/
-(function (w) {
+(function (doc) {
 	
 	// Enable strict mode
 	"use strict";
 
 	// Loop the pictures
 	var map,
-		maps = w.document.getElementsByTagName("img");
+		maps = doc.getElementsByTagName("img");
 	
 	for( var i = 0, il = maps.length; i < il; i++ ){
 		if( maps[i].getAttribute( "data-map" ) !== null ){
@@ -17,8 +17,8 @@
 	}
 
 	// Replace img with dynamic map in iframe
-	w.maphancement = function() {
-		var mapIframe = document.createElement("iframe"),
+	var maphancement = function() {
+		var mapIframe = doc.createElement("iframe"),
 			mapSource = map.src.replace("http://maps.googleapis.com/maps/api/staticmap", "http://maps.google.de/maps").replace("?center", "?q");
 				
 		mapIframe.height = map.height;
@@ -31,12 +31,12 @@
 	if (map) {
 		// Run on click and dragstart
 		if (map.addEventListener) {
-			map.addEventListener("dragstart", w.maphancement, false);
-			map.addEventListener("click", w.maphancement, false);
-		} else if (w.attachEvent) {
-			map.attachEvent ("ondragstart", w.maphancement); 
-			map.attachEvent ("onclick", w.maphancement); 
+			map.addEventListener("dragstart", maphancement, false);
+			map.addEventListener("click", maphancement, false);
+		} else if (map.attachEvent) {
+			map.attachEvent ("ondragstart", maphancement); 
+			map.attachEvent ("onclick", maphancement); 
 		}
 	}
 
-}( this ));
+}( document ));
